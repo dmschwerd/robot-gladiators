b006b5f155609d172bfc2df99db086c4ec4d050c
David <dmsch656@gmail.com>
2023-03-14 10:51:12 -0700

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,6 +75,15 @@ var startGame = function() {
             enemyHealth = 50;
 
             fight(pickedEnemyName);
+
+            if (i < enemyNames.length - 1 && playerHealth > 0) {
+
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round.");
+
+                if (storeConfirm) {
+                    shop();
+                }
+            }
         } else {
             window.alert("You have lost your robot in battle! Game OVer!");
             break;
@@ -97,6 +106,52 @@ var endGame = function() {
         startGame();
     } else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+var shop = function() {
+    // Prompt player for shop options
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+
+    switch (shopOptionPrompt) {
+        case "REFILL":
+        case "refill":
+            if (playerMoney >= 7) {
+                window.alert("Refilling player's health by 20 for 7 dollars.");
+
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+            } else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney >= 7) {
+                window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+            } else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store.");
+
+            break;
+        
+        default:
+            window.alert("You did not pick a valid option.");
+
+            // Go back to start of shop to re-enter prompt
+            shop();
+            break;
     }
 };
 
