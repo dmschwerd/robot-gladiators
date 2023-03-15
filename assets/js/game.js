6230163e0457218510d672e188c1996609492da0
David <dmsch656@gmail.com>
2023-03-15 10:51:12 -0700

    hish score functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -132,12 +132,25 @@ var startGame = function() {
 };
 
 var endGame = function() {
-    if (playerInfo.health > 0) {
-        window.alert("Great job, your robot has survived! You now have a score of " + playerInfo.money + ".");
-    } else {
-        window.alert("You lost your robot in battle.");
+
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // Check localStorage for high score, use 0 if no high score
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
     }
 
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highScore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
+    // Ask player to start another run
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
